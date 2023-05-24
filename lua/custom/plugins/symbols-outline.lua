f17ef4387cfb9ab9e16643ba6b0f06a54011fa4a
Paulo Bortolotti <bortolotti.paulo@gmail.com>
2023-05-24 09:08:28 -0300

    add symbols-outline

--- lua/custom/plugins/symbols-outline.lua
+++ lua/custom/plugins/symbols-outline.lua
@@ -2,5 +2,9 @@ return {
 	'simrat39/symbols-outline.nvim',
 	config = function()
 		require("symbols-outline").setup()
+
+		vim.keymap.set("n", "<leader>o", "<cmd>SymbolsOutline<cr>",
+			{ silent = true, noremap = true }
+		)
 	end
 }
